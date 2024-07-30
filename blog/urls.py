28c82db46f85bda3b224c644eca00ca96f5cb202
Thomas Peace <thomasjpeace29@gmail.com>
2024-07-30 21:35:38 +0100

    add post_list.html

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+# Here we're importing Django's function path and all of our views from the blog application. 
+# (We don't have any yet, but we will get to that in a minute!)
+
+from django.urls import path
+from . import views
+
+
+
+# This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
+
+# The last part, name='post_list', is the name of the URL that will be used to identify the view. 
+# This can be the same as the name of the view but it can also be something completely different. 
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
